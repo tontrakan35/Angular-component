c11ee6e683c49ee24aeb834d7b20cfb554d82fc5
Ton <tontrakan35@hotmail.com>
2020-10-22 14:01:54 +0700

    edit router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routess: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routess)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
